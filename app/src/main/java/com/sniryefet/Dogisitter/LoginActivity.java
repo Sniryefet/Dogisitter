4b57e646d44a0267acad8d26d787a4b006c6c30f
Adi Weisberg <adi.weisberg5@gmail.com>
2019-11-26 16:20:31 +0200

    Retrieve data from database option + tripSearchView new page

--- app/src/main/java/com/sniryefet/Dogisitter/LoginActivity.java
+++ app/src/main/java/com/sniryefet/Dogisitter/LoginActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,12 +22,28 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private EditText mEmailView;
     private EditText mPasswordView;
+    private FirebaseUser userRef;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         Glide.with(this).asGif().load(R.drawable.a_dog_walker_crop).into(imageView);
         mEmailView = findViewById(R.id.login_email);
         mPasswordView = findViewById(R.id.login_password);
-
+        userRef=FirebaseAuth.getInstance().getCurrentUser();
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -68,6 +84,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private void attemptLogin() {
 
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
         String password = mPasswordView.getText().toString();
         String email = mEmailView.getText().toString();
 
@@ -76,25 +96,86 @@ public class LoginActivity extends AppCompatActivity {
 
         Toast.makeText(this,"Login In Progress...",Toast.LENGTH_SHORT).show();
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //User is sign in
+                    Log.d("DogisitterApp", "onAuthStateChange:sign_in: " + user.getUid());
+                }
+                else{
+                    //user is sign-out
+                    Log.d("DogisitterApp", "onAuthStateChange:sign_out");
+                }
+            }
+        };
 
         mAuth.signInWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    if(!task.isSuccessful()){
-                        Log.d("DogisitterApp","Login Failed "+task.getException());
-                        showErrorDialog("There was an error signing in");
-                    }else{
-                         Log.d("DogisitterApp","Login was successful");
-                        //go to the profile page of the admin/client
-                        Intent intent= new Intent(LoginActivity.this,AdminMainActivity.class);
-                        finish();
-                        startActivity(intent);
+                        String perm;
+                        if(!task.isSuccessful()){
+                            Log.d("DogisitterApp","Login Failed "+task.getException());
+                            showErrorDialog("There was an error signing in");
+                        }else{
+                            Log.d("DogisitterApp","Login was successful");
+                            //go to the profile page of the admin/client
+                        }
+
+                        myRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                /**
+                                 * This method is called once with the initial value and again
+                                 * whenever data at this location is updated.
+                                 */
+                                ArrayList<String> userInfo = showData(dataSnapshot);
+                                Log.d("DogisitterApp", "showDataArray: Email: "+ userInfo.get(0));
+                                Log.d("DogisitterApp", "showDataArray: Name: "+ userInfo.get(1));
+                                Log.d("DogisitterApp", "showDataArray: Permission: "+ userInfo.get(2));
+                                Intent intent;
+                                if(userInfo.get(2) == "Admin"){
+                                    intent = new Intent(LoginActivity.this, AdminMainActivity.class);
+                                }else{
+                                    intent = new Intent(LoginActivity.this, TripsViewActivity.class);
+                                }
+                                finish();
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
                     }
-                }
-            });
+                });
 
+
+    }
+
+    private ArrayList<String> showData(DataSnapshot dataSnapshot) {
+        //User userInfo = new User();
+        ArrayList<String> array = new ArrayList<>();
+        for(DataSnapshot ds: dataSnapshot.getChildren()){
+            User uInfo = new User();
+            uInfo.setEmail(ds.child(userID).getValue(User.class).getEmail());
+            uInfo.setName(ds.child(userID).getValue(User.class).getName());
+            uInfo.setPermission(ds.child(userID).getValue(User.class).getPermission());
+            Log.d("DogisitterApp", "showData: Email: "+ uInfo.getEmail());
+            Log.d("DogisitterApp", "showData: Name: "+ uInfo.getName());
+            Log.d("DogisitterApp", "showData: Permission: "+ uInfo.getPermission());
+
+            array.add(uInfo.getEmail());
+            array.add(uInfo.getName());
+            array.add(uInfo.getPermission());
+        }
+        return array;
 
     }
 
