b8d92b50efd3d8e01e294ff287d8136ab527beb1
hemlet <barel987@gmail.com>
2020-01-13 17:53:21 +0200

    last before menu

---
+++ app/src/main/java/com/sniryefet/Dogisitter/ClientTrips.java
@@ -0,0 +1,14 @@
+package com.sniryefet.Dogisitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientTrips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_trips);
+    }
+}
